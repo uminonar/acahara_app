d4343dbdfc624cdcdc068c0484d3b0e169aa8048
uminonar <nupicker@gamil.com>
2016-06-04 17:54:17 +0800

    Assist VCの「相談したい出来事を選択してください」のボタンをタップすると、ChosePostsVCにモーダル遷移。キャンセルと保存で画面が落ちる機能を実装。jsonファイルを取り込みひとまずTableViewに「相談に利用予定」(openFlag = 0のもの）のpostsだけ表示させたかったが、表示がそもそも全くされない。さらにopenFlag=0のものだけpostsに収めるif文がうまくいかなかった。ellipsisBtnと画像を非表示にしたかったが、うまくいっているか、確認ができない。。あとで。postCustomCell.xibと、homeTableViewCell.swiftのファイルを使い回して表示しようとしているが、それで良いのか？

--- acahara_app/ChosePostsViewController.swift
+++ acahara_app/ChosePostsViewController.swift
@@ -10,27 +10,67 @@ import UIKit
 
 class ChosePostsViewController: UIViewController {
 
+    @IBOutlet weak var choseTableView: UITableView!
     var posts:NSMutableArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        choseTableView.registerNib(UINib(nibName: "postCustomCell", bundle: nil), forCellReuseIdentifier: "postCustomCell")
     }
     
     override func viewWillAppear(animated: Bool) {
         let path = NSBundle.mainBundle().pathForResource("posts", ofType: "txt")
         let jsondata = NSData(contentsOfFile: path!)
-        
         let jsonArray = (try! NSJSONSerialization.JSONObjectWithData(jsondata!, options: [])) as! NSArray
-        
-        
-        for data in jsonArray{
-           
-            posts.addObject(data as! NSMutableDictionary)
-            
-        }
 
+        for data in jsonArray{
+            
+            //openFlag=0のものだけここのpostsには収めたい。どうする？
+//            if (data["openFlag"]!="0"){
+            
+                posts.addObject(data as! NSMutableDictionary)
+//            }
+        }
+    }
+    
+    //行数決定
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    //表示内容を決定
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell{
+        
+        // 1. 生成するセルをカスタムクラスへダウンキャスト
+        // 既存のCell生成コードの後に as! <Cellのカスタムクラス名> という記述を追加
+        var cell = tableView.dequeueReusableCellWithIdentifier("postCustomCell", forIndexPath: indexPath) as! homeTableViewCell
+        cell.postCreated.text = posts[indexPath.row]["created"] as! String
+        
+        var dateTime = posts[indexPath.row]["when"] as! String
+        cell.postWhen.text = dateTime+" 頃"
+        cell.postWhere.text = posts[indexPath.row]["where"] as! String
+        cell.postWho.text = posts[indexPath.row]["who"] as! String
+        cell.postUniversity.text = posts[indexPath.row]["university"] as! String
+        cell.postDiary.text = posts[indexPath.row]["diary"] as! String
+        cell.postImageView.image = UIImage(named:(posts[indexPath.row]["picture"] as! String))
+        
+        //どうやったら.xibのイメージを非表示にできる？nilで行ける？
+        cell.postEllipsis.image=nil
+        cell.postImageViewBtn.hidden = true
+        
+        
+        return cell
+    }
+    
+    
+    @IBAction func selectedBtn(sender: UIButton) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    @IBAction func cancelBtn(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     
