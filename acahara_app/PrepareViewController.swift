ef718318bda82f8861ad7df76cba16cd8044db5c
uminonar <nupicker@gamil.com>
2016-06-18 20:05:56 +0800

    no message

---
+++ acahara_app/PrepareViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PrepareViewController.swift
+//  acahara_app
+//
+//  Created by RIho OKubo on 2016/06/18.
+//  Copyright © 2016年 RIho OKubo. All rights reserved.
+//
+
+import UIKit
+
+class PrepareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
